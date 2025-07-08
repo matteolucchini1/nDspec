896ada66a39d15ccd584ec425bfc8c7ff42d8f25
Matteo Lucchini <m.lucchini@uva.nl>
2025-07-08 15:24:38 +0200

    Expanded Xspec library class to actually work, still WiP

--- ndspec/xspec_library.py
+++ ndspec/xspec_library.py
@@ -1,186 +1,67 @@
-import sys
 import ctypes as ct
-import os.path
 import numpy as np
-import matplotlib.pyplot as plt
+from functools import wraps
+import os
 
-type_float_p = ct.POINTER(ct.c_float)
-type_int_p = ct.POINTER(ct.c_int)
+#note: this is not compatible with Relxill specifically, will need a different wrapper urgh
+class XspecLibrary:
+    def __init__(self, lib_path=None):
+        # Default library path
+        if lib_path is None:
+            headas_path = os.environ.get("HEADAS")
+            if headas_path:
+                lib_path = headas_path + f"/../Xspec/{os.path.basename(headas_path)}/lib/libXSFunctions.so"
+            else:
+                raise EnvironmentError("HEADAS environment variable not set.")
 
-#generic wrapper for multiplicative and additive models 
-def xspec_wrap(ear, params, func):
-    '''
-    Takes:
+        # Load the library
+        self.lib = ct.cdll.LoadLibrary(lib_path)
+           
+    def initialize_heasoft(self):
+        init_call = self.lib.fninit_
+        init_call()
 
-    ear   : numpy array of energies
-    params: array of parameters (double)
+def add_linear_model(instance, func):
+    # Strip trailing underscore from the function name
+    func_name = func.__name__.rstrip('_')
 
-    Returns:
+    # Get the library function with the trailing underscore
+    lib_func = getattr(instance.lib, f"{func_name}_")
+    lib_func.argtypes = [
+        ct.POINTER(ct.c_float),
+        ct.POINTER(ct.c_int),
+        ct.POINTER(ct.c_float),
+        ct.POINTER(ct.c_int),
+        ct.POINTER(ct.c_float),
+        ct.POINTER(ct.c_float)
+    ]
+    lib_func.restype = None
 
-    photar: numpy.array (double)
-    '''
+    @wraps(func)
+    def wrapper(ear, params):
+        ne = len(ear) - 1
+        photar = np.zeros(ne, dtype=np.float32)
+        photer = np.zeros(ne, dtype=np.float32)
 
-    ne = len(ear) - 1
+        lib_func(
+            ear.ctypes.data_as(ct.POINTER(ct.c_float)),
+            ct.byref(ct.c_int(ne)),
+            params.ctypes.data_as(ct.POINTER(ct.c_float)),
+            ct.byref(ct.c_int(1)),
+            photar.ctypes.data_as(ct.POINTER(ct.c_float)),
+            photer.ctypes.data_as(ct.POINTER(ct.c_float))
+        )
+        #note: xspec includes a normalization parameter by default
+        #I will need two wrappers - one for multiplicative models which is this one
+        #the other for additive models which renormalize based on the value of the last parameter
+        #thank you xspec!
+        return photar
 
-    photar = np.zeros(ne, dtype = np.float32)
-    photer = np.zeros(ne, dtype = np.float32)
+    # Attach the wrapper to the instance with the name without the underscore
+    setattr(instance, func_name, wrapper)
+    return func
 
-    func(ear.ctypes.data_as(type_float_p),
-         ct.byref(ct.c_int(ne)),
-         params.ctypes.data_as(type_float_p),
-         ct.byref(ct.c_int(1)),
-         photar.ctypes.data_as(type_float_p),
-         photer.ctypes.data_as(type_float_p))
-
-    return photar
-    
-#generic wrapper for convolution models
-def xspec_convolve(ear, seed, params, func):
-    '''
-    Takes:
-
-    ear   : numpy array of energies
-    params: array of parameters (double)
-
-    Returns:
-
-    photar: numpy.array (double)
-    '''
-
-    ne = len(ear) - 1
-
-    seed = np.array(seed,dtype = np.float32)
-    photer = np.zeros(ne, dtype = np.float32)
-
-    func(ear.ctypes.data_as(type_float_p),
-         ct.byref(ct.c_int(ne)),
-         params.ctypes.data_as(type_float_p),
-         ct.byref(ct.c_int(1)),
-         seed.ctypes.data_as(type_float_p),
-         photer.ctypes.data_as(type_float_p))
-
-    return seed
-
-#missing models TBD: slimbh
-
-def load_xspec_library(path):
-    #load the xspec library file
-    lib = ct.cdll.LoadLibrary(path+"/Xspec/x86_64-pc-linux-gnu-libc2.35/lib/libXSFunctions.so")
-    #call fninit ti initialize abundances etc
-    init_call = lib.fninit_
-    init_call()
-    
-    #define all the models - additive models first 
-    agnsed_call = lib.agnsed_
-    agnsed_call.argtypes = [type_float_p, type_int_p, type_float_p, 
-                            type_int_p, type_float_p, type_float_p]
-    agnsed_call.restype  = None
-    def xspec_agnsed(ear, params):
-        return xspec_wrap(ear, params, agnsed_call)  
-        
-    agnslim_call = lib.agnslim_
-    agnslim_call.argtypes = [type_float_p, type_int_p, type_float_p, 
-                             type_int_p, type_float_p, type_float_p]
-    agnslim_call.restype  = None
-    def xspec_agnslim(ear, params):
-        return xspec_wrap(ear, params, agnslim_call)      
-        
-    apec_call = lib.apec_
-    apec_call.argtypes = [type_float_p,  type_int_p, type_float_p, 
-                          type_int_p, type_float_p, type_float_p]
-    apec_call.restype  = None
-    def xspec_apec(ear, params):
-        return xspec_wrap(ear, params, apec_call) 
-    
-    bbodyrad_call = lib.bbodyrad_
-    bbodyrad_call.argtypes = [type_float_p,  type_int_p, type_float_p, 
-                              type_int_p, type_float_p, type_float_p]
-    bbodyrad_call.restype  = None
-    def xspec_bbodrad(ear, params):
-        return xspec_wrap(ear, params, bbodyrad_call) 
-    
-    compps_call = lib.compps_
-    compps_call.argtypes = [type_float_p, type_int_p, type_float_p, 
-                            type_int_p, type_float_p, type_float_p]
-    compps_call.restype  = None
-    def xspec_compps(ear, params):
-        return xspec_wrap(ear, params, compps_call)  
-        
-    diskbb_call = lib.diskbb_
-    diskbb_call.argtypes = [type_float_p,  type_int_p, type_float_p, 
-                            type_int_p, type_float_p, type_float_p]
-    diskbb_call.restype  = None
-    def xspec_diskbb(ear, params):
-        return xspec_wrap(ear, params, diskbb_call) 
-    
-    diskir_call = lib.diskir_
-    diskir_call.argtypes = [type_float_p, type_int_p, type_float_p, 
-                           type_int_p, type_float_p, type_float_p]
-    diskir_call.restype  = None    
-    def xspec_diskir(ear, params):
-        return xspec_wrap(ear, params, diskir_call)  
-    
-    eqpair_call = lib.eqpair_
-    eqpair_call.argtypes = [type_float_p, type_int_p, type_float_p, 
-                            type_int_p, type_float_p, type_float_p]
-    eqpair_call.restype  = None
-    def xspec_eqpair(ear, params):
-        return xspec_wrap(ear, params, eqpair_call)  
-    
-    kerrbb_call = lib.kerbb_
-    kerrbb_call.argtypes = [type_float_p, type_int_p, type_float_p, 
-                             type_int_p, type_float_p, type_float_p]
-    kerrbb_call.restype  = None    
-    def xspec_nthcomp(ear, params):
-        return xspec_wrap(ear, params, kerrbb_call) 
-    
-    nthcomp_call = lib.nthcomp_
-    nthcomp_call.argtypes = [type_float_p, type_int_p, type_float_p, 
-                             type_int_p, type_float_p, type_float_p]
-    nthcomp_call.restype  = None    
-    def xspec_nthcomp(ear, params):
-        return xspec_wrap(ear, params, nthcomp_call)  
-        
-    slimbh_call = lib.slimbh_
-    slimbh_call.argtypes = [type_float_p, type_int_p, type_float_p, 
-                             type_int_p, type_float_p, type_float_p]
-    slimbh_call.restype  = None    
-    def xspec_nthcomp(ear, params):
-        return xspec_wrap(ear, params, slimbh_call) 
-        
-    #then multiplicative models
-    redden_call = lib.redden_
-    redden_call.argtypes = [type_float_p, type_int_p, type_float_p, 
-                            type_int_p, type_float_p, type_float_p]
-    redden_call.restype  = None        
-    def wrap_redden(ear, params):
-        return xspec_wrap(ear, params, redden_call)  
-    
-    tbabs_call = lib.tbabs_
-    tbabs_call.argtypes = [type_float_p, type_int_p, type_float_p, 
-                           type_int_p, type_float_p, type_float_p]
-    tbabs_call.restype  = None        
-    def wrap_tbabs(ear, params):
-        return xspec_wrap(ear, params, tbabs_call)          
-        
-    #then convolution models 
-    reflect_call = lib.reflect_
-    reflect_call.argtypes = [type_float_p, type_int_p, type_float_p, 
-                             type_int_p, type_float_p, type_float_p]
-    reflect_call.restype  = None    
-    def xspec_reflect(ear, seed, params):
-        return xspec_convolve(ear, seed, params, reflect_call)  
-    
-    simpl_call = lib.simpl_
-    simpl_call.argtypes = [type_float_p, type_int_p, type_float_p, 
-                           type_int_p, type_float_p, type_float_p]
-    simpl_call.restype  = None    
-    def xspec_simpl(ear, seed, params):
-        return xspec_convolve(ear, seed, params, simpl_call)  
-    
-    thcomp_call = lib.thcompf_
-    thcomp_call.argtypes = [type_float_p, type_int_p, type_float_p, 
-                            type_int_p, type_float_p, type_float_p]
-    thcomp_call.restype  = None    
-    def xspec_thcomp(ear, seed, params):
-        return xspec_convolve(ear, seed, params, thcomp_call)            
+def register_models(instance, models):
+    for model_name, model_func in models.items():
+        # Apply the decorator to each model function
+        add_linear_model(instance, model_func)
