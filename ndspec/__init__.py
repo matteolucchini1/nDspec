d7ecad7760bf43864e4aab4924bc634176ca0ad9
Ben Ricketts <b.ricketts@sron.nl>
2025-08-19 11:22:33 +0200

    Updated __init__ to make ndspec a regular package

---
+++ ndspec/__init__.py
@@ -0,0 +1,15 @@
+from . import EmceeUtils
+from . import FitCrossSpectrum
+from . import FitPowerSpectrum
+from . import FitTimeAvgSpectrum
+from . import JointFit
+from . import models
+from . import Operator
+from . import Response
+from . import SimpleFit
+from . import Timing
+from . import XspecInterface
+
+__all__ = ["EmceeUtils","FitCrossSpectrum","FitPowerSpectrum","FitTimeAvgSpectrum",
+           "JointFit","models","Operator","Response","SimpleFit","Timing",
+           "XspecInterface"]
